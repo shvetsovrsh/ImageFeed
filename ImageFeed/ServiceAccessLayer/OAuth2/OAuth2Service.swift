7d8ca46ea4b57f3d9fe644f2bdfed97c2b6d8d6e
shvetsovrsh <shvetsovrsh@icloud.com>
2023-06-18 13:29:54 +0300

    removed race in OAuth2Service.fetchOAuthToken

--- ImageFeed/ServiceAccessLayer/OAuth2/OAuth2Service.swift
+++ ImageFeed/ServiceAccessLayer/OAuth2/OAuth2Service.swift
@@ -5,7 +5,8 @@
 import Foundation
 
 final class OAuth2Service {
-
+    private var task: URLSessionTask?
+    private var lastCode: String?
     static let shared = OAuth2Service()
     private let urlSession = URLSession.shared
     private (set) var authToken: String? {
@@ -19,20 +20,31 @@ final class OAuth2Service {
 
 
     func fetchOAuthToken(_ code: String, completion: @escaping (Result<String, Error>) -> Void) {
+        assert(Thread.isMainThread)
+        if lastCode == code {
+            return
+        }
+        task?.cancel()
+        lastCode = code
         let request = authTokenRequest(code: code)
         let task = object(for: request) { [weak self] result in
-            guard let self = self else {
-                return
-            }
-            switch result {
-            case .success(let body):
-                let authToken = body.accessToken
-                self.authToken = authToken
-                completion(.success(authToken))
-            case .failure(let error):
-                completion(.failure(error))
+            DispatchQueue.main.async {
+                guard let self = self else {
+                    return
+                }
+                switch result {
+                case .success(let body):
+                    let authToken = body.accessToken
+                    self.authToken = authToken
+                    completion(.success(authToken))
+                    self.task = nil
+                case .failure(let error):
+                    completion(.failure(error))
+                    self.lastCode = nil
+                }
             }
         }
+        self.task = task
         task.resume()
     }
 }
@@ -45,7 +57,9 @@ private extension OAuth2Service {
         let decoder = JSONDecoder()
         return urlSession.data(for: request) { (result: Result<Data, Error>) in
             let response = result.flatMap { data -> Result<OAuthTokenResponseBody, Error> in
-                Result { try decoder.decode(OAuthTokenResponseBody.self, from: data) }
+                Result {
+                    try decoder.decode(OAuthTokenResponseBody.self, from: data)
+                }
             }
             completion(response)
         }
