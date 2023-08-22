81a0b1531cebe132010f6311e15ab470e6c590fd
shvetsovrsh <shvetsovrsh@icloud.com>
2023-08-22 15:48:45 +0500

    CodingKeys removed

--- ImageFeed/ServiceAccessLayer/OAuth2/OAuth2Service.swift
+++ ImageFeed/ServiceAccessLayer/OAuth2/OAuth2Service.swift
@@ -75,13 +75,6 @@ private extension OAuth2Service {
         let tokenType: String
         let scope: String
         let createdAt: Int
-
-        enum CodingKeys: String, CodingKey {
-            case accessToken = "access_token"
-            case tokenType = "token_type"
-            case scope
-            case createdAt = "created_at"
-        }
     }
 }
 
